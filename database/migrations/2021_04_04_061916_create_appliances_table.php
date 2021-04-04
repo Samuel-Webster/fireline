db0e167c89c453f360e98f27f0c06c06e5b8242f
Sam Webster <sam_webster@live.com>
2021-04-04 16:25:15 +1000

    Added appliances migration & model.

---
+++ database/migrations/2021_04_04_061916_create_appliances_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppliancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appliances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team_id')->index();
+            $table->string('name');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appliances');
+    }
+}
