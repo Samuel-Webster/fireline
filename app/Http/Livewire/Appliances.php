53fd6556e8f1aa64397ce1c487fb0ba7ba524fe1
Sam Webster <sam_webster@live.com>
2021-04-04 17:36:51 +1000

    Started work on CRUD for an appliance.

--- app/Http/Livewire/Appliances.php
+++ app/Http/Livewire/Appliances.php
@@ -7,6 +7,29 @@ use Livewire\Component;
 
 class Appliances extends Component
 {
+    public $showEditModal = false;
+    public ?Appliance $editing;
+
+    protected $rules = [
+        'editing.name' => 'required|min:3',
+    ];
+
+    public function edit(Appliance $appliance)
+    {
+        $this->editing = $appliance;
+
+        $this->showEditModal = true;
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $this->editing->save();
+
+        $this->showEditModal = false;
+    }
+
     public function getAppliancesProperty()
     {
         return Appliance::all();
--- resources/views/livewire/appliances.blade.php
+++ resources/views/livewire/appliances.blade.php
@@ -9,11 +9,40 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 @forelse($appliances as $appliance)
-                <div>{{ $appliance->name }} - {{ $appliance->type }}</div>
+                <div>
+                    {{ $appliance->name }} - {{ $appliance->type }}
+                    <x-jet-secondary-button wire:click="edit({{ $appliance->id }})" wire:loading.attr="disabled">
+                        Edit
+                    </x-jet-secondary-button>
+                </div>
                 @empty 
                 No Appliances
                 @endforelse
             </div>
         </div>
     </div>
+
+    {{-- Modals --}}
+    <form wire:submit.prevent="save">>
+        <x-jet-dialog-modal wire:model="showEditModal">
+            <x-slot name="title">
+                Edit Appliance
+            </x-slot>
+        
+            <x-slot name="content">
+                <x-jet-input type="text" wire:model="editing.name"/>
+                <x-jet-input-error for="editing.name"/>
+            </x-slot>
+        
+            <x-slot name="footer">
+                <x-jet-secondary-button wire:click="$toggle('confirmingUserDeletion')" wire:loading.attr="disabled">
+                    Cancel
+                </x-jet-secondary-button>
+        
+                <x-jet-button class="ml-2" wire:loading.attr="disabled">
+                    Save
+                </x-jet-button>
+            </x-slot>
+        </x-jet-dialog-modal>
+    </form>
 </div>
