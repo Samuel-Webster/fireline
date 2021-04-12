910f6d657f4671f8e02f61501f7c014bc83fe7c3
Sam Webster <sam_webster@live.com>
2021-04-12 16:02:15 +1000

    Fixed bad file.

--- app/Models/Appliance.php
+++ app/Models/Appliance.php
@@ -9,4 +9,58 @@ class Appliance extends Model
 {
     use HasFactory;
 
-    protecte
+    protected $guarded = [];
+
+    protected $casts = [
+        'year' => 'date:Y-m-d',
+    ];
+
+    const TYPES = [
+        'light',
+        'medium',
+        'heavy',
+        'support',
+        'control'
+    ];
+
+    const MAKES = [
+        'Isuzu',
+        'Hino',
+        'Ford',
+        'Nissan',
+        'Toyota',
+        'Iveco',
+        'Man',
+        'Scania',
+    ];
+
+    public function logs()
+    {
+        return $this->hasMany(ApplianceLog::class);
+    }
+
+    public function checklist()
+    {
+        return $this->hasMany(ApplianceChecklist::class);
+    }
+
+    public function checklistLogs()
+    {
+        return $this->hasMany(ApplianceChecklistLog::class);
+    }
+
+    public function getCurrentOdometerAttribute()
+    {
+        return number_format($this->logs()->orderBy('odometer_in', 'DESC')->first()->odometer_in ?? 0);
+    }
+
+    public function getlastLogTimeInAttribute()
+    {
+        return $this->logs()->orderBy('time_in', 'DESC')->first()->time_in ?? null;
+    }
+
+    public function getlastCheckedAttribute()
+    {
+        return $this->checklistLogs()->orderBy('created_at', 'DESC')->first()->created_at ?? null;
+    }
+}
